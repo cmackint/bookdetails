b067329d78f9696390aac659cf325b59fe51081e
Cam <oatmilkcam@posteo.net>
2022-10-21 20:03:35 -0700

    Initial commit
    Created project using Spring Initializr
    
    Added example GraphQL schema

---
+++ src/test/java/net/oatmilkcam/springbootgraphql/SpringbootGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package net.oatmilkcam.springbootgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
